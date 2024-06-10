33475964ab0326c6920b76ddd56305e65d9a7301
Jojohn Tabal <tabal.jojohn@gmail.com>
2024-06-10 17:38:07 +0800

    profile update

--- src/views/WorkPage.vue
+++ src/views/WorkPage.vue
@@ -4,16 +4,16 @@
     <div class="row">
       <div class="offset-lg-2 col-lg-8 mt-3" align="center">
         <div class="col-lg-10 w-full">
-          <div class="card shadow-xl p-2">
+          <div class="card shadow-xl p-5">
             <div class="row">
-              <div class="col-4 p-3">
+              <div class="col-4">
                 <img
                   class="img-fluid"
                   src="/images/projects/Zoning Evaluation System.jpg"
                   style="width: 700px; height: auto"
                 />
               </div>
-              <div class="col-8 px-3">
+              <div class="col-8">
                 <h2 class="text-[24px] leading-[2.50rem]" align="left">
                   <strong>Zoning Evaluation System</strong>
                 </h2>
@@ -33,23 +33,16 @@
     <div class="row">
       <div class="offset-lg-2 col-lg-8 mt-3" align="center">
         <div class="col-lg-10 w-full">
-          <div class="card shadow-xl p-2">
+          <div class="card shadow-xl p-2 h-[250px]">
             <div class="row">
-              <div class="col-4 p-3">
-                <img
-                  class="img-fluid"
-                  src="/images/projects/Zoning Evaluation System.jpg"
-                  style="width: 700px; height: auto"
-                />
-              </div>
-              <div class="col-8 px-3">
+              <div class="col-12 p-5">
                 <h2 class="text-[24px] leading-[2.50rem]" align="left">
                   <strong>Moneylend</strong>
                 </h2>
                 <p class="p-0" align="left"><small>2020</small></p>
                 <p align="left">
-                  A freelancing website that lets you post jobs and apply for
-                  one.
+                  A lending management web application that's intended to only
+                  work locally on the client's computer.
                 </p>
               </div>
             </div>
